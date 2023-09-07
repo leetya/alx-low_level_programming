a4a90a38d05a26a5410885cb7aa67c0fafb45632
eyassine <leetyacine@gmail.com>
2023-09-07 23:07:16 +0100

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+* main - this is the entry point
+* Description: this is a program that print a string to the stderr. using write
+* Return: the program always returns 1.
+*/
+
+#include <unistd.h>
+
+int	main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
