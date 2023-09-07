f68b64c3dba0e9802b00afb73d63865acf5801fc
eyassine <leetyacine@gmail.com>
2023-09-07 22:50:54 +0100

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+* main - this is the entry point
+* Description: this is a program that print a string. using printf
+* Return: the function always returns 0.
+*/
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
