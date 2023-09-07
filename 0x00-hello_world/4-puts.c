056416f5dc6f5617a08754b4198e8346c2e407f2
eyassine <leetyacine@gmail.com>
2023-09-07 22:47:38 +0100

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+* main - this is the entry point
+* Description: this is a program that print a string.
+* Return: the function always returns 0.
+*/
+
+#include <stdio.h>
+
+int	main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
