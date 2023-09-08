0bbb23e39ca6336ce01b8d5552f3370922636f47
eyassine <leetyacine@gmail.com>
2023-09-08 21:04:24 +0100

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - this is the entry point
+* Description: this is a program that check a number sign.
+* Return: the function always returns 0.
+*/
+
+int	main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+		putchar(a++);
+	a = 'A';
+	while (a <= 'Z')
+		putchar(a++);
+	putchar('\n');
+	return (0);
+}
