30c137348b9002705c82f1c04d3d22cf3447fc73
eyassine <leetyacine@gmail.com>
2023-09-08 20:58:36 +0100

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - this is the entry point
+* Description: this is a program that check a number sign.
+* Return: the function always returns 0.
+*/
+
+int	main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+		putchar(a++);
+}
