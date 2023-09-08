2d3ebba57f3e183d62843aa3ea2ec8cec3c7c263
eyassine <leetyacine@gmail.com>
2023-09-08 22:10:05 +0100

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - this is the entry point
+* Description: this is a program that check a number sign.
+* Return: the function always returns 0.
+*/
+
+int	main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+		putchar(a--);
+	putchar('\n');
+	return (0);
+}
