d70d6011c2f50e5cc94d30986c525433fa90372e
eyassine <leetyacine@gmail.com>
2023-09-12 15:44:40 +0100

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * print_sign - prints the sign of a number.
+ * @n: the value to be checked.
+ * Return: 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else
+		_putchar('-');
+	return (0);
+}
