5bc67df31a5ca076984ed35b528ba7dc553da99e
eyassine <leetyacine@gmail.com>
2023-09-12 14:57:10 +0100

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 /**
+ * ft_puts - print a string in the stdout
+ * @str: the string to be printed.
+*/
+
+void	ft_puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
+/**
 * main - this is the entry point
 * Description: this is a program that print a string.
 * Return: the function always returns 0.
@@ -8,6 +22,6 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	ft_puts("_putchar");
 	return (0);
 }
