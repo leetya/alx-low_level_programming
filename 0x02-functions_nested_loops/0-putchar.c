76bdc1fdf5101adcf3eb317b404e7a38544331d2
eyassine <leetyacine@gmail.com>
2023-09-12 14:46:49 +0100

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - this is the entry point
+* Description: this is a program that print a string.
+* Return: the function always returns 0.
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
