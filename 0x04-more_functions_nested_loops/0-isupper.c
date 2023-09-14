b36b5904f6367ef4b72a3fa946dacdc8a8cf0ec1
eyassine <leetyacine@gmail.com>
2023-09-14 17:48:01 +0100

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper -  a function that checks for uppercase character.
+ * @c: the value to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
