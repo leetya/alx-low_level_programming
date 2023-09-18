c8aa39e72d23f0492c91ec370765599e16a95539
eyassine <leetyacine@gmail.com>
2023-09-18 13:14:43 +0100

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: the string
+*/
+
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+}
