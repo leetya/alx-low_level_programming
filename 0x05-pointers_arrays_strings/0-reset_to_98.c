cfbe6214e45dac2238ffce13a2bede61cbd35a8d
eyassine <leetyacine@gmail.com>
2023-09-18 10:16:50 +0100

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - modify the value n points to.
+ * @n: a pointer to int
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(int);
+void reset_to_98(int *);
+
+
+#endif
