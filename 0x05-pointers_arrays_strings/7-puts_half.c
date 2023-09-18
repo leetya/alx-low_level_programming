56c5e5e55d2e956bc421e6b2d3bfe113592c171c
eyassine <leetyacine@gmail.com>
2023-09-18 13:21:04 +0100

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,43 @@
 #include "main.h"
 
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str: string.
+*/
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * puts_half - print the second half of the string
+ * @str: the string
+*/
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+
+	_puts(str + (len / 2));
+}
