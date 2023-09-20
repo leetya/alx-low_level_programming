9d11c4703b45fcd74747d453989a8eef7712e5aa
eyassine <leetyacine@gmail.com>
2023-09-20 20:39:03 +0100

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string1
+ * @s2: string2
+ * Return: int value
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
