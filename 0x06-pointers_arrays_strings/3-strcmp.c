c86f521d2d36eece54a26b7ef6e512a6339ef367
eyassine <leetyacine@gmail.com>
2023-09-20 20:43:18 +0100

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    return(*s1 - *s2);
+	int res;
+
+	res = 0;
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+    return (res);
 }
