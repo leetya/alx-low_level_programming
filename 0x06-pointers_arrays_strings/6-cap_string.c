d783a0997f09dd158421cd342be3d0ffd852a499
eyassine <leetyacine@gmail.com>
2023-09-22 16:20:42 +0100

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * _isupper -  a function that checks for uppercase character.
+ * @c: the value to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _isalpha - check if c a letter
+ * @c: the value to be checked
+ * Return: 1 if c a letter, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
+
+/**
+ * separator - check if it is a separator
+ * @c: the value to be checked
+ * Return: 1 if true 0 if false
+*/
+
+int separator(int c)
+{
+	if (c == ',' || c == ';' || c == '.' || c == '!' || c == '?'
+	|| c == 34 || c == '(' || c == ')'
+	|| c == '{' || c == '}' || c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalise all words of a string
+ * @s: the string
+ * Return: the string itslef.
+*/
+
+char *cap_string(char *s)
+{
+	int i, cap;
+
+	cap = 1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!cap && _isalpha(s[i]) && !_isupper(s[i]))
+			s[i] -= 32;
+		cap = 1;
+		if (separator(s[i]))
+			cap = 0;
+	}
+	return (s);
+}
