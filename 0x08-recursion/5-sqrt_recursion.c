7bd3482a4ebc0392c6c79ceca348d6fdf743b06d
eyassine <leetyacine@gmail.com>
2023-09-28 22:20:01 +0100

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * calc_square - a helper function
+ * @n: the original value
+ * @i: an iterator to calculate the square.
+ * Return: the value
+*/
+
+int	calc_square(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	return (calc_square(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - calculate sqaure using recursion
+ * @n: the value to be calculated
+ * Return: the value
+*/
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i * i == n)
+		return (i);
+	return (calc_square(n, 0));
+}
