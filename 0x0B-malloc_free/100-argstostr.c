221a0296c77c83e37b093fb6bd5e24146de88211
eyassine <leetyacine@gmail.com>
2023-10-04 22:59:49 +0100

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * argstostr - a function that concatenates all the arguments of your program.
+ * @ac: the number of arguments
+ * @av: the arguments
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int len = 0;
+	char *new;
+	
+	if (!ac || !av)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		len += (_strlen(av[i]) + 1);
+	new = malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (NULL);
+	len = 0;
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			new[len] = av[i][j];
+			len++;
+		}
+		new[len++] = '\n';
+	}
+	new[len] = '\0';
+	return (new);
+}
+
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
