5f228e5d4d8652cda602707d2c7c089958f03903
eyassine <leetyacine@gmail.com>
2023-10-08 16:23:05 +0100

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *p;
-    int i;
+	char *p;
+	int i;
 
-    if (!nmemb || !size)
-        return NULL;
-    p = malloc(nmemb * size);
-    if (!p)
-        return (NULL);
-    for (i = 0; i < nmemb * size; i++)
-        p[i] = 0;
-    return (p);
+	if (!nmemb || !size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return (p);
 }
