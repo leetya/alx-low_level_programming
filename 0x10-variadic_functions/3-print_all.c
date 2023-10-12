414a6b112665c2a90a3299e72b7e123a334d4633
El Kemmal Yassine <eyassine@e3r7p2.1337.ma>
2023-10-12 18:04:03 +0100

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_all - a function that prints anything
- * @format: is a list of types of arguments passed to the function 
+ * @format: is a list of types of arguments passed to the function
  */
 
 void print_all(const char * const format, ...)
