b2294e9b10e4d4dccb17c501c68ec027d05e0f85
El Kemmal Yassine <eyassine@e3r7p2.1337.ma>
2023-10-13 21:29:07 +0100

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
+ * get_op_func - ***
+ * @s: ***
+ * Return: int
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
