e3dd0ad75d3603872cd04cd2ba9811511d916c8c
El Kemmal Yassine <eyassine@e3r7p2.1337.ma>
2023-10-13 21:11:16 +0100

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,19 @@
  * Return: the index of the match or -1 if no match
  */
 
-int	int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	int	i;
+	int i;
 
-	if (size <= 0 || !array || !cmp) 
-		return (-1);
+	if (size <= 0)
+	return (-1);
+	if (array == NULL || cmp == NULL)
+	return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) == 1)
-			return (i);
+	if (cmp(array[i]) == 1)
+	return (i);
 	}
 	return (-1);
 }
