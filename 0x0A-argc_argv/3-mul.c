2c590c1d90d1ce5be61e820ae6ffdf4f2a2c733a
eyassine <leetyacine@gmail.com>
2023-11-17 23:39:01 +0100

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @ac: args count
+ * @av: args val
+ * Return: 0
+*/
+
+int main(int ac, char **av)
+{
+    if (ac != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    printf("%d\n", atoi(av[1]) * atoi(av[2]));
+	return (0);
+}
