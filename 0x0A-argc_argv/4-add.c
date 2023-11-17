82086b66372caebeee726cdab0c75b24bf0f25e3
eyassine <leetyacine@gmail.com>
2023-11-18 00:10:59 +0100

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * _isdigit - a function that checks for a digit (0 through 9).
+ * @c: the value
+ * Return: 1 if c is a digit, else 0.
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * main - prints its name
  * @ac: args count
  * @av: args val
@@ -13,15 +26,15 @@ int main(int ac, char **av)
 
 	if (ac == 1)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
 	for (i = 1; i < ac; i++)
 	{
 		j = 0;
 		for (j = 0; av[i][j]; j++)
 		{
-			if (!isdigit(av[i][j]))
+			if (!_isdigit(av[i][j]))
 			{
 				printf("Error\n");
 				return (1);
