aac7a6d90cb22c0cd25edf872831fcaa17554b00
eyassine <leetyacine@gmail.com>
2023-11-17 22:35:59 +0100

    a script that creates a static library from all the .c files that are in the current directory.

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	(void)s;
+	(void)accept;
+	return (0);
+}
