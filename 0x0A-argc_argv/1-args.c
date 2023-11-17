ccbdf81ee36455b5e734713b3ad49d3a9fd3af00
eyassine <leetyacine@gmail.com>
2023-11-17 23:29:19 +0100

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @ac: args count
+ * @av: args val
+ * Return: 0
+*/
+
+int main(int ac, char **av)
+{
+	(void)av;
+
+	printf("%d\n", ac);
+	return (0);
+
+}
