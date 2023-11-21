f8771b2fa56a4ae3d51d3557fc86021efac533aa
eyassine <leetyacine@gmail.com>
2023-11-21 22:16:32 +0100

    4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,10 +11,7 @@
 
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
-	}
+	for (; height >= 0; height--)
+		free(grid[height]);
+	free(grid);
 }
