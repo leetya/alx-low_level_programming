bf54f2cb489477e963984ef064476ec88746cb39
eyassine <leetyacine@gmail.com>
2023-11-24 20:55:44 +0100

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: max number of chars
+ * Return: pointer of an array of chars
+ */
+
+char *str_concat(char *s1, char *s2, unsigned int n)
+{
+	char *new;
+	int s1_len, s2_len, k, i;
+
+	if (!s1 || !s2)
+	{
+		if (!s1)
+			s1 = "";
+		if (!s2)
+			s2 = "";
+	}
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+	if (s2_len < n)
+		n = s2_len;
+	new = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (k = 0; k < s1_len; k++)
+		new[k] = s1[k];
+	i = s2_len;
+	for (s2_len = 0; s2_len <= i && n; s2_len++)
+	{
+		new[k++] = s2[s2_len];
+		n--;
+	}
+	return (new);
+}
