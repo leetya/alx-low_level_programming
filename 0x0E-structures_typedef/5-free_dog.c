a940757f649d35c1732e6e4b1e7592a3cc64fdef
eyassine <leetyacine@gmail.com>
2023-11-27 23:27:33 +0100

    3. Betty is killing me

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - frees
@@ -14,4 +15,13 @@ void free_dog(dog_t *d)
 	if (d->owner)
 		free(d->owner);
 	free(d);
+}
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
+    return (0);
 }
