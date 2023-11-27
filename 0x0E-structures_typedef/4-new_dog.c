4bf97a6588b32164a9083fc87d832db36caaceb6
eyassine <leetyacine@gmail.com>
2023-11-27 23:09:31 +0100

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,57 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strdup - duplicate
+ * @old: str to be dup
+ * Return: p
+*/
+
+char *_strdup(char *old)
+{
+	char *new;
+
+	new = malloc(_strlen(old));
+	if (!new)
+		return (NULL);
+	return (new);
+}
+
+/**
+ * new_dog - func to create a new dog.
+ * @name: its name
+ * @age: its age;
+ * @owner: its owner;
+ * Return: p to new dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	if (!name || !owner)
+		return (NULL);
+	new = malloc(sizeof(dog_t));
+	if (!new)
+		return (NULL);
+	new->name = _strdup(name);
+	new->owner = _strdup(owner);
+	if (!new->name || !new->owner)
+		return (NULL);
+	return (new);
+}
