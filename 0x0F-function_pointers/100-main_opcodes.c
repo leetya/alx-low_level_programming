eda053b2bbd6a1c3556698cda5a65b29c97d6b6d
eyassine <leetyacine@gmail.com>
2023-11-30 00:38:31 +0100

    100-lower case

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,33 +2,41 @@
 #include <stdlib.h>
 
 /**
- * main - print opcode
- * @ac: args count
- * @av: args vals;
- * Return: 0;
-*/
-
-int main(int ac, char **av)
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
 	int bytes, i;
-	char *add;
+	char *arr;
 
-	if (ac != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(av[1]);
+
+	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	add = (char *)main;
+
+	arr = (char *)main;
+
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%02hhx ", add[i]);
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
-	printf("\n");
 	return (0);
 }
