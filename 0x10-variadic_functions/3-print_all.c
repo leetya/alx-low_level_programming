8cec5c70aa57e344644ee206a006c7a92a8ad02c
eyassine <leetyacine@gmail.com>
2023-11-30 22:54:35 +0100

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include "variadic_functions.h"
+
+/**
+ * display_char - display format
+ * @p: valist p
+*/
+
+void display_char(va_list *p)
+{
+	printf("%c", va_arg(*p, int));
+}
+
+/**
+ * display_int - display format
+ * @p: valist p
+*/
+
+void display_int(va_list *p)
+{
+	printf("%d", va_arg(*p, int));
+}
+
+/**
+ * display_float - display format
+ * @p: valist p
+*/
+
+void display_float(va_list *p)
+{
+	printf("%f", va_arg(*p, double));
+}
+
+/**
+ * display_str - display format
+ * @p: valist p
+*/
+
+void display_str(va_list *p)
+{
+	char *s = va_arg(*p, char *);
+
+	printf("%s", s ? s : "(nil)");
+}
+
+/**
+ * print_all - function that prints anything
+ * @format: a list of types of arguments passed to the function
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list arg_p;
+	int i, j;
+	display_t display[4] = {
+		{'c', display_char},
+		{'i', display_int},
+		{'f', display_float},
+		{'s', display_str}
+	};
+
+	va_start(arg_p, format);
+	i = 0;
+	while (format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == display[j].value)
+			{
+				display[j].p(&arg_p);
+				if (format[i + 1] != '\0')
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct display - Struct display
+ * @value: The type
+ * @p: a pointer to The function associated
+ */
+
+typedef struct display
+{
+	char value;
+	void (*p)(va_list *);
+} display_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
