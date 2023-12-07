bf8e94b873ba569337ea13dc9147eb335906bc77
eyassine <leetyacine@gmail.com>
2023-12-08 00:25:57 +0100

    2bett

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - a function that returns the length of a string.
- * @s: the string to be counted.
- * Return: the length of the string
-*/
-
-unsigned int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len])
-		len++;
-	return (len);
-}
-
-/**
  * add_node - ass
  * @head: ptohead
  * @str: str_val
@@ -25,20 +10,17 @@ unsigned int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	size_t nb;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
-
 	new->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	new->len = nchar;
+	nb = 0;
+	while (str[nb])
+		nb++;
+	new->len = nb;
 	new->next = *head;
 	*head = new;
-
 	return (*head);
 }
