9c33924c50061657d6180f0acbf59db1d35d4bd2
eyassine <leetyacine@gmail.com>
2023-12-08 00:24:01 +0100

    2bett

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,13 +25,20 @@ unsigned int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
-	new->len = _strlen(new->str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
 	new->next = *head;
 	*head = new;
+
 	return (*head);
 }
