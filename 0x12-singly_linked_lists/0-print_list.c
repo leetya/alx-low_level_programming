97ebaf49fd60ca37d37ea5d0264ffd2e2de17618
eyassine <leetyacine@gmail.com>
2023-12-07 23:35:54 +0100

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * @print_list - prints
+ * @h: linked list
+ * Return: size_t 
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nb;
+
+	if (!h)
+		return (0);
+	nb = 0;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+		{
+			printf("[%d] ", h->len);
+			printf("%s\n", h->str);
+		}
+		h = h->next;
+	}    
+	return (nb);
+}
