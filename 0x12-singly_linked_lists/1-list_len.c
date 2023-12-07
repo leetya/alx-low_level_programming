b35b4e1d458a8249f8a29768c2093c63517c7d4f
eyassine <leetyacine@gmail.com>
2023-12-07 23:43:19 +0100

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - length
+ * @h: linked list
+ * Return: size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+    size_t nb;
+
+    if (!h)
+        return (0);
+    for (nb = 0; h; h = h->next)
+        nb++;
+}
