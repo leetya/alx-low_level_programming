de2062d71c6f2644653cde24e7e8aecbada42734
eyassine <leetyacine@gmail.com>
2023-12-11 21:59:01 +0100

    6 pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,12 @@
 
 int pop_listint(listint_t **head)
 {
-    int nb = 0;
+	int nb = 0;
 
-    if (!head)
-        return (nb);
-    nb = (*head)->n;
-    *head = (*head)->next;
-    return (nb);
+	if (!head)
+		return (nb);
+	nb = (*head)->n;
+	if ((*head)->next)
+		*head = (*head)->next;
+	return (nb);
 }
