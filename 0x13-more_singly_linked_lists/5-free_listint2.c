5d6de81f1d777a2507cc38094edc5939588be4e2
eyassine <leetyacine@gmail.com>
2023-12-11 21:46:16 +0100

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (head)
+	while (*head)
 	{
 		tmp = (*head)->next;
-		free(head);
-		head = tmp;
+		free(*head);
+		*head = tmp;
 	}
 	*head = NULL;
 }
