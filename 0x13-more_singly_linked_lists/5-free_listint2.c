71d4393e2fb8172a14454fa4d17f6d84b03123f9
eyassine <leetyacine@gmail.com>
2023-12-11 21:40:30 +0100

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees and set to null
+ * @head: fnode
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = (*head)->next;
+		free(head);
+		head = tmp;
+	}
+	*head = NULL;
+}
