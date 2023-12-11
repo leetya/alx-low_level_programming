96587f50a97a36453b19c233f33aa7e6873629bd
eyassine <leetyacine@gmail.com>
2023-12-11 23:09:10 +0100

    9 test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -59,7 +59,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head || !*head)
 		return (NULL);
 	tmp = *head;
-	if (idx >= listint_len(*head))
+	if (idx > listint_len(*head))
 		return (NULL);
 	if (!idx)
 		return (add_nodeint(head, n));
