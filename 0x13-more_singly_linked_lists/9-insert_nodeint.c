2b2b4dc3d8fdf3945ef0ff4f13531790c4d26789
eyassine <leetyacine@gmail.com>
2023-12-11 22:54:10 +0100

    9 test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,28 @@
 #include "lists.h"
 
+#include "lists.h"
+
+/**
+ * add_nodeint - add
+ * @head: ptohead
+ * @n: str_val
+ * Return: listint_t*
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+
 /**
  * insert_nodeint_at_index -  inserts a new node at a given position.
  * @head: list
@@ -20,13 +43,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+	if (!idx)
+	{
+		add_nodeint(head, n);
+		return (new);
+	}
 	new->n = n;
-	while (*head && idx - 1)
+	while (*head && idx - 1 > 0)
 	{
 		idx--;
 		*head = (*head)->next;
 	}
-	if (!idx - 1 || !idx)
+	if (!idx - 1)
 	{
 		tmp2 = (*head)->next;
 		(*head)->next = new;
