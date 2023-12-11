a73614135e9538c2624c34a55dc81702c1cf8cf3
eyassine <leetyacine@gmail.com>
2023-12-11 22:19:25 +0100

    8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all
+ * @head: list
+ * Return: sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
