be579fe2117a3b7283ec02522ba2fd28521c4acd
eyassine <leetyacine@gmail.com>
2023-12-14 23:40:09 +0100

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string to be counted.
+ * Return: the length of the string
+*/
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _pow_recursion - the value of x raised to the power of Y
+ * @x: the number
+ * @y: the power
+ * Return: x to the power of y
+*/
+
+unsigned int _pow_recursion(unsigned int x, unsigned int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, --y));
+}
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int pow = 0, res = 0;
+	int len;
+
+	if (!b)
+		return (0);
+	len = _strlen(b);
+	len--;
+	while (len != -1)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] != '0')
+			res += _pow_recursion(2, pow);
+		pow++;
+		len--;
+	}
+	return (res);
+}
+
