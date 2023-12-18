6a471030ade116c01668d3dae0ce161d77c471ed
eyassine <leetyacine@gmail.com>
2023-12-19 00:47:45 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,14 +27,17 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int chars;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	chars = write(fd, text_content, _strlen(text_content));
-	if (chars != _strlen(text_content))
-		return (-1);
+	if (text_content)
+	{
+		chars = write(fd, text_content, _strlen(text_content));
+		if (chars != _strlen(text_content))
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
