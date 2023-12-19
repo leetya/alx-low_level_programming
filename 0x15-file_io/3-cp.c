cdfa7eb52e9862d59fb79e8440de12588301486a
eyassine <leetyacine@gmail.com>
2023-12-19 01:40:43 +0100

    3test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,10 +24,7 @@ void	close_error(int fd_closed, int fd)
 
 void	_error(char *error, char *filename, int exit_status)
 {
-	if (!filename)
-		dprintf(STDERR_FILENO, error);
-	else
-		dprintf(STDERR_FILENO, error, filename);
+	dprintf(STDERR_FILENO, error, filename);
 	exit(exit_status);
 }
 
@@ -44,13 +41,16 @@ int main(int ac, char **av)
 	char *buff;
 
 	if (ac != 3)
-		_error("Usage: cp file_from file_to\n", NULL, 97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd_in = open(av[1], O_RDONLY);
 	if (fd_in < 0)
-		_error("Error: Can't read from file %s\n", av[1], 98);
+		_error("Error: Can't read from file\n", av[1], 98);
 	fd_out = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd_out < 0)
-		_error("Error: Can't write to %s\n", av[2], 99);
+		_error("Error: Can't write to\n", av[2], 99);
 	buff = malloc(sizeof(char) * 1025);
 	if (!buff)
 		return (-1);
